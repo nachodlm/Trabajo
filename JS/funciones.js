ad18d08977459ec735e005d02597b7e6ea84611c
nachodlm <117827381+nachodlm@users.noreply.github.com>
2022-12-09 09:31:35 +0100

    Add files via upload

---
+++ JS/funciones.js
@@ -0,0 +1,9 @@
+/**********JS QUE REGULA LA BARRA DE NAVEGACION******/
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
